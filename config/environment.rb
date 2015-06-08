c312bd1882ead8dcdfcf1e027780cfea6d27c524
Torey Hickman <torey@toreyhickman.com>
2015-06-08 17:37:06 -0500

    Generalize db name

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,7 @@ ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 # Configure the database
 database_config = { :adapter  =>  "sqlite3",
-                    :database => "#{APP_ROOT}/db/dogs.sqlite3" }
+                    :database => "#{APP_ROOT}/db/#{APP_ROOT}-database.sqlite3" }
 
 ActiveRecord::Base.establish_connection(database_config)
 
