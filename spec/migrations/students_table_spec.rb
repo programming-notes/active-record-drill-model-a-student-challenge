12dfcccf236503edec7fd0433ad4f16bf49445db
Torey Hickman <torey@toreyhickman.com>
2015-06-08 19:04:00 -0500

    Update student table specs

--- spec/migrations/students_table_spec.rb
+++ spec/migrations/students_table_spec.rb
@@ -1,12 +1,13 @@
 require_relative '../spec_helper'
 
-describe "create table with correct schema" do
-  it "has a Students table" do
-    expect(ActiveRecord::Base.connection.table_exists?(:students)).to be_truthy
+describe "database students table" do
+  it "has a students table" do
+    students_table = ActiveRecord::Base.connection.table_exists?(:students)
+    expect(students_table).to be_truthy
   end
 
-  it "has the right columns and types" do
-    expected_column_types_and_columns = {
+  it "has the right columns of the right types" do
+    expected_column_types_and_names = {
       :integer  => ["id"],
       :string   => ["first_name", "last_name", "phone"],
       :date     => ["birthday"],
